d31efa730dc142e90860233f2676f06cd944686b
Demiin <ch3rnov.denis@gmail.com>
2015-05-13 16:46:06 +0300

    Initial commit

---
+++ bcrossNE/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   main.cpp
+ * Author: demiin
+ *
+ * Created on May 13, 2015, 4:44 PM
+ */
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
